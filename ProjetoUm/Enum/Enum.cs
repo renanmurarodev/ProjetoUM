27ac711231876af7d99cda3fa846dad05a1d7a1d
renanmurarodev <renanmura.dev@gmail.com>
2021-11-26 01:26:45 -0300

    Adicionei o Enum

---
+++ ProjetoUm/Enum/Enum.cs
@@ -0,0 +1,31 @@
+namespace  ProjetoUm
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Arte = 3,
+        Chanchada = 4,
+        Comedia = 5,
+        Casos = 6,
+        Drama = 7,
+        Romantica = 8,
+        Danca = 9,
+        Documentario = 10,
+        Docuficcao = 11,
+        Espionagem = 12,
+        Faroeste = 13,
+        Fantasia = 14,
+        Cientifica = 15,
+        Ficcao = 16,
+        Truques = 17,
+        Guerra = 18,
+        Misterio = 19,
+        Musical = 19,
+        Policial = 20,
+        Romance = 21,
+        Terror = 22,
+        Thriller = 23,
+
+    }
+}
